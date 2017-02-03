dd6d9e1be8e9ae94253b63f4c6387450a2518ca4
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-02-03 15:52:28 -0500

    almost there

--- main.js
+++ main.js
@@ -2,11 +2,46 @@ console.log("YOOO")
 
 
 var timerEl = document.querySelector(".timer")
-var counterVal= 25
+var counterVal= 1500
+var completeEl= document.querySelector(".complete")
+var textBox= document.querySelector(".output-textbox")
+// console.log(completeEl)
+
+
 
 var countBack= setInterval(function(){
+  var seconds=counterVal
+  var minutes= Math.floor(counterVal/60);
+  var secondsLeft=counterVal-(minutes*60)
+  if (secondsLeft < 10){
+    secondsLeft= "0" + secondsLeft;
+  }
   counterVal--
-  timerEl.innerHTML=counterVal
+  timerEl.innerHTML=minutes+ ":" + secondsLeft
     if(counterVal === 0 ) {
-      clearInterval(countBack)}
-}, 1000)
+      clearInterval(countBack)
+      completeEl.style.fontSize="40px"
+      timerEl.style.display="none"
+      textBox.style.display="none"
+      }
+}, 10)
+
+var inputValue=document.querySelector(".input-textbox input")
+var buttonEl= document.querySelector(".input-textbox button")
+var outputEl=document.querySelector(".output-textbox")
+
+buttonEl.addEventListener("click", function(evt){
+  outputEl.innerHTML=inputValue.value
+})
+
+
+
+// var timerEl = document.querySelector(".timer")
+// var counterVal= 25
+//
+// var countBack= setInterval(function(){
+//   counterVal--
+//   timerEl.innerHTML=counterVal
+//     if(counterVal === 0 ) {
+//       clearInterval(countBack)}
+// }, 1000)
